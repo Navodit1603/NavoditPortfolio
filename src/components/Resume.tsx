cb86677bdbe22800740a7d10566f52050d2c0974
Navodit1603 <navoditmaheshwari@gmail.com>
2025-11-03 12:49:52 -0800

    update work experience

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -2,18 +2,18 @@ import Timeline from "./Timeline";
 
 const workExperience = [
     {
+        role: "Computer Architecture Class Tutor",
+        date: "September 2024 - Present",
+        company: "Baskin Engineering School, UCSC",
+        description:
+            "Tutored students in computer architecture topics such as CPU design, memory hierarchy, pipelining, and ISA, while leading discussions on performance trade-offs. Provided feedback on assembly code and collaborated with faculty to support over 30 students across multiple sections.",
+    },
+    {
         role: "Computer Architecture Class Grader",
         date: "March 2025 - June 2025",
         company: "Baskin Engineering School, UCSC",
         description:
             "Worked with professors and TAs to grade assignments and exams for the Computer Architecture class. Provided feedback to students on their work and helped clarify concepts.",
-    },
-    {
-        role: "Computer Architecture Class Tutor",
-        date: "September 2024 - June 2025",
-        company: "Baskin Engineering School, UCSC",
-        description:
-            "Tutored students in computer architecture topics such as CPU design, memory hierarchy, pipelining, and ISA, while leading discussions on performance trade-offs. Provided feedback on assembly code and collaborated with faculty to support over 30 students across multiple sections.",
     },
     {
         role: "Coding Instructor",
